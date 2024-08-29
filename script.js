2e11daba568103d91862f9bcf180698a1c0b384d
Arthur Granito <arthurcursotecnico@gmail.com>
2024-08-29 15:27:39 -0300

    agora sim o commit final!

--- script.js
+++ script.js
@@ -6,21 +6,27 @@ const botaoLimpar = document.getElementById('botaoLimpar');
 const gorjetaPorcentagem = document.getElementById('outra');
 const resultado = document.getElementById('resultado')
 let porcentagem;
+const pErro = document.getElementById('erro');
 const botaoCalcular = document.getElementById('calcular');
 
 botaoCalcular.addEventListener('click', () => {
-    if(conta.value == '' || pessoas.value == '' || gorjetaPorcentagem.value == 0){
-        alert('Preencha os campos');
-        conta.value = '';
-        pessoas.value = '';
-        gorjetaPorcentagem.value = '';
+    if(pessoas.value == '0'){
+        pErro.style.display = 'block';
     } else{
-        porcentagem = gorjetaPorcentagem.value / 100;
-        resultado.style.display = 'block';
-
-        gorjetaPrecoResultado.innerText = `R$ ${((conta.value * porcentagem) / pessoas.value).toFixed(2)}`;
-
-        totalPessoaResultado.innerText = `R$ ${((parseFloat(conta.value) + (parseFloat(conta.value) * porcentagem)) / pessoas.value).toFixed(2)}`;
+        pErro.style.display = 'none';
+        if(conta.value == '' || pessoas.value == '' || gorjetaPorcentagem.value == 0){
+            alert('Preencha os campos');
+            conta.value = '';
+            pessoas.value = '';
+            gorjetaPorcentagem.value = '';
+        } else{
+            porcentagem = gorjetaPorcentagem.value / 100;
+            resultado.style.display = 'block';
+    
+            gorjetaPrecoResultado.innerText = `R$ ${((conta.value * porcentagem) / pessoas.value).toFixed(2)}`;
+    
+            totalPessoaResultado.innerText = `R$ ${((parseFloat(conta.value) + (parseFloat(conta.value) * porcentagem)) / pessoas.value).toFixed(2)}`;
+        }
     }
 })
 
